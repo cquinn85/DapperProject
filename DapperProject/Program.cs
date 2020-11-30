adf034c82a1788dbfbba34acb3eb40ff1f7df3fd
cquinn85 <cequinn300@gmail.com>
2020-11-30 13:54:14 -0600

    Add project files.

---
+++ DapperProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
